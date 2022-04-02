376bb63d77e7c7f5b91b5fa84b8d28dcd696813a
Daniel Fucci <daniel.m.fucci@gmail.com>
2022-04-02 10:00:40 -0300

    My first commit

---
+++ src/main/java/com/fucci/watchman/WatchmanApplication.java
@@ -0,0 +1,13 @@
+package com.fucci.watchman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchmanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchmanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fucci/watchman/WatchmanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fucci.watchman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchmanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
